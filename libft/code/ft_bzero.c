a71874be7102300a9831ff1976155733a52d9da6
ouassim-behlil <bahlil2001@gmail.com>
2025-09-30 19:07:14 +0100

    add functions

---
+++ libft/code/ft_bzero.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*cs;
+
+	i = 0;
+	cs = (char *)s;
+	while (i < n)
+		cs[i++] = '\0';
+}
---
+++ libft/code/ft_strlcpy.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t dsize)
+{
+	size_t	i;
+	size_t	sLen;
+
+	sLen = ft_strlen(src);
+	if (dsize == 0)
+		return (sLen);
+	i = 0;
+	while (i < dsize - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (sLen);
+}
